87486a8c0f87212d6ce2157ebd9fbfa7f723adf2
TejasMore2022 <tejasmore02@gmail.com>
2025-10-29 12:40:36 +0530

    Added first commot

---
+++ src/GroupCall.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class GroupCall {
+
+	public static void main(String[] args) {
+
+		String st="W";
+		
+		
+
+		char st1[]=st.toCharArray();
+
+
+String st2="";
+		for(int i=0;i<st1[i];i++){
+
+		st2=st2+st1[i];
+
+		System.out.println(st2);
+
+
+	}
+
+	}
+}
